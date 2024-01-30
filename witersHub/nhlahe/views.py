1d2d13baa1bbd0f70939c2336beb580eec6ab128
Mokoena2000 <100692323+Mokoena2000@users.noreply.github.com>
2024-01-30 19:25:37 +0200

    set up my signup page

--- witersHub/nhlahe/views.py
+++ witersHub/nhlahe/views.py
@@ -1,6 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 from django.http import HttpResponse
+from .models import Profile
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def signup(request):
+    
+    if request.method == 'POST':
+        email = request.POST['email']
+        username = request.POST['username']
+        password = request.POST['password']
+        confirmpassword = request.POST['confirmpassword']
+        
+        if password == confirmpassword:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email Taken')
+                return redirect('signup')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username Taken')
+                return redirect('signup')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+                
+                #Log user in and direct to settings page
+                
+                #Create profile object for new user
+                user_model = User.objects.get(username=username)
+                new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
+                new_profile.save()
+                return redirect('signup')
+        else:
+            messages.info(request, 'Ooops Passwords Not Matching')
+            return redirect('signup')
+    else:
+       return render(request, 'signup.html')
