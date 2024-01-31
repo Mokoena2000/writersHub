daf911892b52241ead5e87d74cff41f187a9e174
Mokoena2000 <100692323+Mokoena2000@users.noreply.github.com>
2024-01-31 16:43:44 +0200

    set up authentication setttings and fixed bugs

--- witersHub/nhlahe/views.py
+++ witersHub/nhlahe/views.py
@@ -11,6 +11,36 @@ from .models import Profile
 def index(request):
     return render(request, 'index.html')
 
+@login_required(login_url='signin')
+def settings(request):
+    user_profile = Profile.objects.get(user=request.user)
+    
+    if request.method == 'POST':
+        
+      if request.FILES.get('image') == None:
+          image = user-profile.profileimg
+          bio = request.POST['bio']
+          location = request.POST['location']
+          
+          user_profile.profileimg = image
+          user_profile.bio = bio
+          user_profile.location = location
+          user_profile.save()
+      if request.FILES.get('image') != None:
+          image = request.FILES.get('image')
+          bio = request.POST('bio')
+          location = request.POST('location')
+          
+          user_profile.profileimg = image
+          user_profile.bio = bio
+          user_profile.location = location
+          user_profile.save()
+          
+      return redirect('settings')
+            
+    
+    return render(request, 'settings.html', {'user_profile': user_profile})
+
 def signup(request):
     
     if request.method == 'POST':
@@ -31,12 +61,13 @@ def signup(request):
                 user.save()
                 
                 #Log user in and direct to settings page
-                
+                user_login = auth.authenticate(username=username, password=password)
+                auth.login(request, user_login)
                 #Create profile object for new user
                 user_model = User.objects.get(username=username)
                 new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
                 new_profile.save()
-                return redirect('signup')
+                return redirect('settings')
         else:
             messages.info(request, 'Ooops Passwords Not Matching')
             return redirect('signup')
