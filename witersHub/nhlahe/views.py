d14f8bcd67589a3b98cf4f43827c6ecf618380fd
Mokoena2000 <100692323+Mokoena2000@users.noreply.github.com>
2024-02-01 16:04:18 +0200

    fixed up minor bugs and problems keeping profile from displaying

--- witersHub/nhlahe/views.py
+++ witersHub/nhlahe/views.py
@@ -9,7 +9,13 @@ from .models import Profile
 
 @login_required(login_url='signin')
 def index(request):
-    return render(request, 'index.html')
+    user_object = User.objects.get(username=request.user.username)
+    user_profile = Profile.objects.get(user=user_object)
+    return render(request, 'index.html', {'user_profile': user_profile})
+
+@login_required(login_url='signin')
+def upload(request):
+    return HttpResponse('<h1>Upload View</h1>')
 
 @login_required(login_url='signin')
 def settings(request):
