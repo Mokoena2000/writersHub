98ffe219f77a41daa4df3cbe814c1620d90320c9
Mokoena2000 <100692323+Mokoena2000@users.noreply.github.com>
2024-02-06 11:36:42 +0200

    set op function to allow users to like another users post

--- witersHub/nhlahe/urls.py
+++ witersHub/nhlahe/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('logout', views.logout, name= 'logout'),
     path('settings', views.settings, name= 'settings'),
     path('upload', views.upload, name='upload'),
+    path('like-post', views.like_post, name='like-post'),
 ]
--- witersHub/nhlahe/views.py
+++ witersHub/nhlahe/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from .models import Profile
+from .models import Profile, Post, LikePost
 
 # Create your views here.
 
@@ -12,8 +12,8 @@ def index(request):
     user_object = User.objects.get(username=request.user.username)
     user_profile = Profile.objects.get(user=user_object)
     
-    pst = post.object.all()
-    return render(request, 'index.html', {'user_profile': user_profile, 'posts' :posts})
+    post = post.object.all()
+    return render(request, 'index.html', {'user_profile': user_profile, 'posts': posts})
 
 @login_required(login_url='signin')
 def upload(request):
@@ -30,6 +30,28 @@ def upload(request):
     else:
         return redirext('/')
 
+@login_required(login_url='signin')
+def like_post(request):
+    username = request.user.username
+    post_id = request.GET.get('post_id')
+    
+    post = Post.objects.get(id=post_id)
+    
+    like_filter = LikePost.objects.filter(post_id=post_id, username=username).first
+   
+    if like_filter ==None:
+        new_like = LikePost.objects.create(post_id=post_id, username=username)
+        new_like.save()
+        post.no_of_likes = post.no_of_likes+1
+        post.save()
+        return redirect('/')
+    else:
+        like_filter.delete()
+        post.no_of_likes = post.no_of_likes-1
+        post.save()
+        return redirect('/')
+        
+        
 @login_required(login_url='signin')
 def settings(request):
     user_profile = Profile.objects.get(user=request.user)
